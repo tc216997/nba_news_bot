c6ca905975017fe7ab6b2211c722752d6e2dce84
Terry <tc216997@gmail.com>
2021-02-17 20:32:35 +0800

    removed config.js

--- app.js
+++ app.js
@@ -1,27 +1,21 @@
-const Discord = require('discord.js');;
+require('dotenv').config();
+const Discord = require('discord.js');
 const bot = new Discord.Client();
 const Twit = require('twit');
-const configs = require('./config.js');
+
 const Twitter = new Twit({
-  consumer_key: configs.consumer_key,
-  consumer_secret:configs.consumer_secret,
-  access_token: configs.access_token,
-  access_token_secret: configs.access_token_secret
+  consumer_key: process.env.consumer_key,
+  consumer_secret:process.env.consumer_secret,
+  access_token: process.env.access_token,
+  access_token_secret: process.env.access_token_secret
 })
 
 bot.on('ready', (message) => {
   console.log('on!')
-  //bot.channels.cache.get('627344566562848805').send('https://twitter.com/FantasyLabsNBA/status/1361134610793586689');
-  
-  //https://twitter.com/[screen name of user]/status/[id of status]
-  //FantasyLabsNBA twitter id = 3444040513
-
   const stream = Twitter.stream('statuses/filter', { follow: '3444040513' })
   stream.on('tweet', function (tweet) {
     //console.log(`https://www.twitter.com/${tweet.user.screen_name}/status/${tweet.id}`);
-    //https://twitter.com/FantasyLabsNBA/status/1361134610793586689
     // if link dont show up on preview, try using www infront instead
-
     let news = `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id}`
     bot.channels.cache.get('627344566562848805').send(news);
   });
@@ -31,6 +25,6 @@ bot.on('ready', (message) => {
 
 
 //TODO: change this to use configs.token in the future
-bot.login(configs.bot_token);
+bot.login(process.env.bots_token);
 
-
+//process.env.API_KEY, process.env.API_SECRET, process.env.PASSPHRASE
