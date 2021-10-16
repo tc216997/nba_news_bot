a5ab9310ab0d81f4cfb2f8333226639003729973
terry <tc216997@gmail.com>
2021-10-16 11:32:55 +0800

    updated twitter

--- app.js
+++ app.js
@@ -12,13 +12,13 @@ const Twitter = new Twit({
 
 bot.on('ready', (message) => {
   console.log('on!')
-  const stream = Twitter.stream('statuses/filter', { follow: '3444040513' })
+  const stream = Twitter.stream('statuses/filter', { follow: '1448762998840074240' })
   stream.on('tweet', function (tweet) {
     //`https://www.twitter.com/${tweet.user.screen_name}/status/${tweet.id}`
     // if link dont show up on preview, try using www infront instead
 
     let news = `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`
-    if (tweet.user.screen_name === 'FantasyLabsNBA') {
+    if (tweet.user.screen_name === 'Underdog__NBA') {
       //console.log(tweet)
       bot.channels.cache.get('627344566562848805').send(news);
     }
